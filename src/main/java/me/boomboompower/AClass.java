2b0e5b617f389eb3e0a6bd6cf23690bd99bdc5a4
boomboompower <boomboompowermc@gmail.com>
2016-04-22 16:31:17 +1000

    Initial commit

--- src/main/java/me/boomboompower/AClass.java
+++ src/main/java/me/boomboompower/AClass.java
@@ -1,9 +1,12 @@
 package me.boomboompower;
 
-import org.bukkit.JavaPlugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
+/**
+ * Created by ThePowerSquad on 22-Apr-16.
+ */
 public class AClass extends JavaPlugin {
-    
+
     @Override
     public void onEnable() {
         new AListener(this);
--- src/main/java/me/boomboompower/AListener.java
+++ src/main/java/me/boomboompower/AListener.java
@@ -1,19 +1,25 @@
 package me.boomboompower;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+/**
+ * Created by ThePowerSquad on 22-Apr-16.
+ */
+
 public class AListener implements Listener {
 
-    private AClass aClass;
-    public AListener(AClass aClass) {
-        this.aClass = aClass;
-        Bukkit.getPluginManager().registerEvents(this, aClass);
-    }
+    private AClass aClass;
+    public AListener(AClass aClass) {
+        this.aClass = aClass;
+        Bukkit.getPluginManager().registerEvents(this, aClass);
+    }
 
-    @EventHandler
-    private void onPlayerJoin(PlayerJoinEvent e) {
-        e.setJoinMessage(ChatColor.GREEN + "+ " + ChatColor.DARK_GREEN + e.getPlayer().getName());
-    }
+    @EventHandler
+    private void onPlayerJoin(PlayerJoinEvent e) {
+        e.setJoinMessage(ChatColor.GREEN + "+ " + ChatColor.DARK_GREEN + e.getPlayer().getName());
+    }
 }
